4ab600829f86c41636790a0540a53d301d38e2b9
Sahithyavangala <sahithyavangala86@gmail.com>
2025-04-08 16:27:23 +0530

    committed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
 	
-	uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "/graphql", // the URL of our GraphQL server.
+	uri: import.meta.env.VITE_NODE_ENV === "development" ? "http://localhost:4000/graphql" : "https://expense-tracker-app-n1fd.onrender.com/graphql", // the URL of our GraphQL server.
 	cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
 	credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
